c06ea1381a3dd027d795e329297ba9cc70eecf1a
Jack36VRN <jack3@JACK36-LAPTOP>
2019-11-19 13:49:37 +0300

    q

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	$new_product = 'test';
-	$host = 'boge.mysql:3306';
-	$user = 'boge_mysql';
-	$pass = 'uEhFQN8+';
-	$db_name = 'boge_admin_db';
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db_name = 'boge';
 
 	$domain = 'ru';
 
